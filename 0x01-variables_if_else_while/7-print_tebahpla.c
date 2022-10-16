bb62703963c4173b5c5980f09c720fea663e47fb
Deborahanyaji <anyajideborah@gmail.com>
2022-10-16 14:02:23 -0700

    hello

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'z'
+; letter <= 'a'
+; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
+
