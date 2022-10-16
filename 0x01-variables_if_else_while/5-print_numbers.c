233648a283255ffd79b8557042331c2b7a30db5f
Deborahanyaji <anyajideborah@gmail.com>
2022-10-16 11:05:41 -0700

    hello

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main numbers of base
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0'
+; number <= 10
+; number++)
+	printf("%d"
+, number);
+	printf("\n");
+	return (0);
+}
