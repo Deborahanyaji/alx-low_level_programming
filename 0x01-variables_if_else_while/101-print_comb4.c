ef42afcf2f0f9cb7eea2d39b11323cd28c77df4e
Deborahanyaji <anyajideborah@gmail.com>
2022-10-17 09:00:48 -0700

    advance 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 <= 9; digit1++)
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
 	{
-	for (digit2 = 0; digit2 <= 9; digit2++)
+	for (digit2 = '0'; digit2 <= '9'; digit2++)
 	{
-		for (digit3 = 0; digit3 <= 9; digit3++)
+		for (digit3 = '0'; digit3 <= '9'; digit3++)
 		{
 			if (digit2 > digit1 && digit3 > digit2)
 			{
 				putchar(digit1);
 				putchar(digit2);
 				putchar(digit3);
-				if (digit1 != 7 && digit2 != 8 && digit3 != 9)
+				if (digit1 != '7' && digit2 != '8' && digit3 != '9')
 				{
 					putchar(',');
 					putchar(' ');
