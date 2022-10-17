fb1782a30dd2ae9be522b3a5b1d02b74c843e8f7
Deborahanyaji <anyajideborah@gmail.com>
2022-10-17 07:33:57 -0700

    hello

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - numbe
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int number1, number2
+
+	for (number1 = 0
+; number1 <= 98
+; number1++)
+
+	for (number2 = number1 + 1
+; number2 <= 99
+; number2++)
+	putchar((number1 / 1) + '0');
+	putchar((number2 / 10) + '0');
+	putchar((numbere / 10) + '0');
+(number1 == 98
+&& number2 == 99)
+continue;
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
