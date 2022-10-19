6cbcc9a97263750cec4bf8f4d522c1228df05da4
Deborahanyaji <anyajideborah@gmail.com>
2022-10-18 22:52:25 -0700

    debby

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * _islower - check if character c is lower
  *Return: 1 if chars is a lowercase otherwise return 0
  */
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
