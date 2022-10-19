73ae4241faf036178a67a6387953be2bce88d7b9
Deborahanyaji <anyajideborah@gmail.com>
2022-10-19 10:54:31 -0700

    debby yet

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int hour, int minutes;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			putchar((hour / 10) + '0');
+			putchar((hour % 10) + '0');
+			putchar(':');
+			putchar((minutes / 10) + '0');
+			putchar((minutes % 10) + '0');
+			putchar('\n');
+		}
+
+	}
+}
