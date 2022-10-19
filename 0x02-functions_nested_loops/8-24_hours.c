73ada560c073d3f7d5204cbfd389e07f841bd64c
Deborahanyaji <anyajideborah@gmail.com>
2022-10-19 11:01:17 -0700

    debby yet

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,16 +4,16 @@
  */
 void jack_bauer(void)
 {
-	int hour, int minutes;
+	int hour, minutes;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
-			putchar((hour / 10) + '0');
+			putchar((hour \ 10) + '0');
 			putchar((hour % 10) + '0');
 			putchar(':');
-			putchar((minutes / 10) + '0');
+			putchar((minutes \ 10) + '0');
 			putchar((minutes % 10) + '0');
 			putchar('\n');
 		}
