0d00debc22be5efd511566a805dcb2b6f80af5cc
Deborahanyaji <anyajideborah@gmail.com>
2022-10-19 08:06:16 -0700

    debby c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - the last digit of a number.
+ * @n: last_digit(int)
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (0);
+}
