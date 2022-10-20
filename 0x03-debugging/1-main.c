a0a5e62f2f1cd480b0b2e09030845df8bdcac488
Deborahanyaji <anyajideborah@gmail.com>
2022-10-20 04:16:42 -0700

    gg

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,8 @@ int main(void)
 {
 	 int i;
 
-	 printf("Infinite loop incoming :(\n");
+	 printf("
+Infinite loop incoming :(\n");
 
 	 i = 0;
 
@@ -20,7 +21,8 @@ int main(void)
 	*}
 	*/
 
-	printf("Infinite loop avoided! \\o/\n");
+	printf("
+Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
