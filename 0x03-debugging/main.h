906a928628109876038a944bd4a450085ae3df15
Deborahanyaji <anyajideborah@gmail.com>
2022-10-20 20:33:37 -0700

    gg

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,11 +5,13 @@
 
 int _putchar(char c);
 
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c);
+
 void positive_or_negative(int i);
 
 void print_remaining_days();
-int largest_number(int a, int b, int c)
+
+largest_number(int a, int b, int c)
 
 int convert_day(int month, int day);
 
