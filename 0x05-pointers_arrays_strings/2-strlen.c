38179e8ff2873d5281ab639ced62647a320d2ded
Deborahanyaji <anyajideborah@gmail.com>
2022-10-24 14:14:15 -0700

    ggg

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string pointer for the string
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int p = 0;
+
+	/*increment up to the last character is NULL, \0*/
+	while (*(s + p) != 0)
+	{
+		p++
+	}
+		return (p);
+}
+
