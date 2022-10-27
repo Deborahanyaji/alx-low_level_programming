be3487113bf990d33a47adf33dac7167a54c2d56
Deborahanyaji <anyajideborah@gmail.com>
2022-10-26 22:45:02 -0700

    ggggg

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - functions to copy a string
+ * @dest: pointer
+ * @src: pointer
+ * @n: variable integer
+ * Return: 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[a + b]  = src[b];
+	}
+		return (dest);
+}
