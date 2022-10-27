e032865b4f91df9b66552e4e2bdefb3c6aca174a
Deborahanyaji <anyajideborah@gmail.com>
2022-10-27 14:58:33 -0700

    ggg

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * root13 - function to convert to root13 code
+ * @strs: string
+ * Return: strs
+ */
+char *rot13(char *strs)
+{
+
+	char *alpha_1 = "
+ABCDEFGHIJKLMNOPQRSTUVWXY
+abcdefghijklmnopqrstuvwxyz"
+;
+
+	char *alpha _2 = "
+NOPQRSTUVWXYZABCDEFGHIJKLM
+nopqrstuvwxyzabcdefghijklm"
+;
+
+	int a, b;
+
+	for (a = 0; strs[a] != '\0'; a++)
+	{
+	for (b = 0; alpha_1[b] != '\0'; b++)
+	{
+	if (strs[a] == alpha_1[b])
+	{
+	strs[a] = alpha_2[b];
+
+	break;
+				}
+			}
+	}
+		return (strs);
+}
+
