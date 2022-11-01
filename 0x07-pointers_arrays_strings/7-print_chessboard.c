bd25331b3dfe25296b5ec314cd84146dd0fe037e
Deborahanyaji <anyajideborah@gmail.com>
2022-11-01 02:16:12 -0700

    gg

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,12 @@
 void print_chessboard(char (*a)[8])
 {
 	int indx1, indx2;
-	int _putchar
 
 	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
 		for (indx2 = 0; indx2 < 8; indx2++)
 			
-			_putchar(a[indx1][indx2]);
+			putchar(a[indx1][indx2]);
 	}
-				_putchar('\n');
+				putchar('\n');
 }
