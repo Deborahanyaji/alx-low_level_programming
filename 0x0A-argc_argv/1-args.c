c6ef98ac8209b774b0271e70b3a9defbad49025d
Deborahanyaji <anyajideborah@gmail.com>
2022-11-05 14:42:23 -0700

    ggg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * Return: 0
  * argv: argumenys passed here.
  */
-int main(int argc, char __attribute__((used)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _putchar(char c);
 int main(int argc, char *argv[]);
+int main(int argc, char __attribute__((used)) *argv[]);
 #endif
